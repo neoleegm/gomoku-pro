54014216f3e08f7d35e86af95db362ad3500aa65
neoleegm <neo.xp.lee@gmail.com>
2026-04-14 14:54:29 +0800

    按需加载统计脚本

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { initAnalytics } from "./lib/analytics";
 import App from "./App";
 import "./index.css";
+
+initAnalytics();
 
 createRoot(document.getElementById("root")!).render(<App />);
